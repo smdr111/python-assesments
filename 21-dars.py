62bc1881b073475e96e872a8efe266ae13d11e51
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2025-05-11 11:34:23 -0400

    Add files via upload

---
+++ 21-dars.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 30 02:17:11 2025
+
+@author: samandaroripov
+"""
+# MODULLAR
+
+# import modul_nomi / modul_nomi.funksiya_nomi
+
+# modulga qisqa nom berish: as / import modul_nomi as mn
+
+# moduldan bazi funksiyalarni kochirib olish:
+# from modul_nomi import funksiya1,funksiya2
+
+# funksiylarga qisqa nom berish: 
+# from modul_nomi import funksiya1 as f1,funksiya2 as f2
+
+# Modul ichidagi barcha funksiyalarni ko`chirib olish:
+# from modul_nomi import *
+
+# Modulda o`zgaruvchi saqlash:
+# Modullarning ichida nafaqat funksiyalar, 
+# balki turli o'zgaruvchilarni ham saqlash mumkin. 
+# Modul ichidagi o'zgaruvchilarga ham huddi yuqoridagi 
+# usullar bilan murojat qilish mumkin.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
