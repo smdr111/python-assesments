c778fd47276c1d51efe73c67112484fc15e77e29
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2025-05-15 11:46:57 -0400

    Add files via upload

---
+++ 23-dars.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 11 11:29:56 2025
+
+@author: samandaroripov
+"""
+# Inkapsulatsiya
+
+
+
+
+from uuid import uuid4
+class Avto:
+    """Avtomobil klassi"""
+    def __init__(self,make,model,rang,yil,narh,km=0):
+        """Avtomobilning xususiyatlari"""
+        self.make = make
+        self.model = model
+        self.rang = rang
+        self.yil = yil
+        self.narh = narh
+        self.__km = km
+        self.__id = uuid4()
+    
+    def get_km(self):
+        return self.__km
+    
+    def get_id(self):
+        return self.__id
+    def add_km(self,km):
+        if km>=0:
+            self.__km += km
+        else:
+            print("Mashina km kamaytirib bo'lmaydi")
+
+avto1 = Avto("GM","Malibu","Qora",2020,40000,100000)
+
+class Avto:
+    """Avtomobil klassi"""
+    num_avto = 0
+    def __init__(self,make,model,rang,yil,narh,km=0):
+        """Avtomobilning xususiyatlari"""
+        self.make = make
+        self.model = model
+        self.rang = rang
+        self.yil = yil
+        self.narh = narh
+        self.__km = km
+        self.__id = uuid4()
+        Avto.num_avto += 1
+avto1 = Avto("GM","Malibu","Qora",2020,40000)
+avto2 = Avto("GM","Lacetti","Oq",2020,20000)
+print(avto1.num_avto)
+
+# Class ga oid metodlar
+class Avto:
+    """Avtomobil klassi"""
+    __num_avto = 0
+    def __init__(self,make,model,rang,yil,narh,km=0):
+        """Avtomobilning xususiyatlari"""
+        self.make = make
+        self.model = model
+        self.rang = rang
+        self.yil = yil
+        self.narh = narh
+        self.__km = km
+        self.__id = uuid4()
+        Avto.__num_avto += 1
+    
+    @classmethod
+    def get_num_avto(cls):
+        return cls.__num_avto
+
+class Shaxs:
+    """Shaxslar haqida ma'lumot"""
+    def __init__(self,ism,familiya,passport,tyil):
+        self.ism = ism
+        self.familiya = familiya
+        self.passport = passport
+        self.__tyil = tyil
+    
+    def get_info(self):
+        """Shaxs haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya}. "
+        info += f"Passport:{self.passport}, {self.tyil}-yilda tug`ilgan"
+        return info
+        
+    def get_age(self,yil):
+        """Shaxsning yoshini qaytaruvchi metod"""
+        return yil - self.tyil
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
